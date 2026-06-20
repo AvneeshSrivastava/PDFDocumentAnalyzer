40b80432a4f02daba78d32c15aae4ee810ba11ee
Avn <Avn@flowtrics.com>
2026-06-20 17:40:51 +0530

    feat(ui): add spinner

--- static/js/site.js
+++ static/js/site.js
@@ -1,7 +1,50 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleButton = document.getElementById("sidebar-toggle");
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Sidebar
+    const sidebarBtn = document.getElementById("sidebar-toggle");
     const sidebar = document.getElementById("sidebar");
-    toggleButton.addEventListener("click", () => {
-        sidebar.classList.toggle("sidebar-collapsed");
-    });
+
+    if (sidebarBtn && sidebar) {
+
+        sidebarBtn.addEventListener("click", function () {
+
+            sidebar.classList.toggle("sidebar-collapsed");
+
+        });
+
+    }
+
+    // Upload Spinner
+    const form = document.getElementById("uploadForm");
+    const spinner = document.getElementById("uploadSpinner");
+
+    if (form && spinner) {
+
+        form.addEventListener("submit", function () {
+
+            spinner.classList.remove("d-none");
+
+        });
+
+    }
+
 });
+
+const uploadButton = document.getElementById("uploadButton");
+
+if (form && spinner) {
+
+    form.addEventListener("submit", function () {
+
+        spinner.classList.remove("d-none");
+
+        if (uploadButton) {
+
+            uploadButton.disabled = true;
+            uploadButton.innerText = "Processing...";
+
+        }
+
+    });
+
+}
