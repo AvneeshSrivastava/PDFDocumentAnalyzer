f0d7ab21264296fa6cea96982aaef177ab01100d
Avn <Avn@flowtrics.com>
2026-06-20 15:45:13 +0530

    feat(ui): prepare layout for collapsible sidebar

--- src/web/app.py
+++ src/web/app.py
@@ -5,9 +5,10 @@ from src.exceptions.handlers import pdf_exception_handler
 from src.logging import logger
 from src.web.routes.pdf_routes import router
 from src.web.routes.health_routes import router as health_router
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.add_exception_handler(
     PDFAnalyzerException,
     pdf_exception_handler
